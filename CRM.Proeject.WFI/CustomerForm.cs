99cabfd3882cc44330e52b6bf5fd51f7445e6da1
catdog50rus <catdog50rus@gmail.com>
2019-08-28 13:03:37 +0300

    какойто фикс

--- CRM.Proeject.WFI/CustomerForm.cs
+++ CRM.Proeject.WFI/CustomerForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace CRM.Proeject.WFI
 {
-    public partial class Form1 : Form
+    public partial class CustomerForm : Form
     {
-        public Form1()
+        public CustomerForm()
         {
             InitializeComponent();
         }
